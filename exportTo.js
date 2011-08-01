16ab3187bdca5e4f09d42dac474722a4be937045
Norman Xu <homyu.shinn@gmail.com>
2011-08-01 15:06:17 +0800

    support jquery.fn.end()

--- exportTo.js
+++ exportTo.js
@@ -1,101 +1,105 @@
 /*!require: */
 !function($){
-	'use strict';
-	function embedCSS(i, el){
-		var styles = el.currentStyle || el.ownerDocument.defaultView.getComputedStyle(el);
-		var value, realKey, 
-			// on modern browser, we can only iterate numeric keys and 
-			// by accessing numeric keys, we can get the real css attribute name
-			// but in ie, we will iterate css attribute name directly
-			numericKey = styles.length == null? false: true;
-		
-		for(var key in styles){
-			// style does not has hasOwnProoperty on ie < 9
-			if (styles.hasOwnProperty && !styles.hasOwnProperty(key)){
-				continue;
-			}
-			
-			// do not copy over the entire cssText
-			if (key === 'cssText' || key === 'length'){
-				continue;
-			}
-			
-			realKey = numericKey?styles[key]:key;
-			value = styles[realKey];
-			
-			if (value == null){
-				continue;
-			}
-			
-			el.style[realKey] = value;
-		}
-	};
-	function importNode(externalNode){
-		var attr, style, key, value;
-		var adopted = this.createElement(externalNode.nodeName);
+    'use strict';
+    function embedCSS(i, el){
+        var styles = el.currentStyle || el.ownerDocument.defaultView.getComputedStyle(el);
+        var value, realKey,
+            // on modern browser, we can only iterate numeric keys and
+            // by accessing numeric keys, we can get the real css attribute name
+            // but in ie, we will iterate css attribute name directly
+            numericKey = styles.length == null? false: true;
+
+        for(var key in styles){
+            // style does not has hasOwnProoperty on ie < 9
+            if (styles.hasOwnProperty && !styles.hasOwnProperty(key)){
+                continue;
+            }
+
+            // do not copy over the entire cssText
+            if (key === 'cssText' || key === 'length'){
+                continue;
+            }
+
+            realKey = numericKey?styles[key]:key;
+            value = styles[realKey];
+
+            if (value == null){
+                continue;
+            }
+
+            el.style[realKey] = value;
+        }
+    };
+    function importNode(externalNode, deep){
+        var attr, style, key, value;
+        var adopted = this.createElement(externalNode.nodeName);
         // copy the attributes of the source element
-		for(var l = externalNode.attributes.length; l--; ){
-			attr = externalNode.attributes[l];
-			if (attr.specified){
-				if (attr.name === 'style'){
-					style = externalNode.style;
-					for(key in style){
-						try{
-							value = style[key];
-							if (value != null && value != false){
-								adopted.style[key] = value;
-							}
-						}
-						catch(ex){
-							if (window.console){
-								window.console.log('exportTo plugin failed to copy css: ' + key);
-							}
-						}
-					}
-				}
-				else{
-					adopted.setAttribute(attr.name, attr.value);
-				}
-			}
-		}
-		
-        // copy the entire contents of the source element
-        adopted.innerHTML = externalNode.innerHTML;
+        for(var l = externalNode.attributes.length; l--; ){
+            attr = externalNode.attributes[l];
+            if (attr.specified){
+                if (attr.name === 'style'){
+                    style = externalNode.style;
+                    for(key in style){
+                        try{
+                            value = style[key];
+                            if (value != null && value != false){
+                                adopted.style[key] = value;
+                            }
+                        }
+                        catch(ex){
+                            if (window.console){
+                                window.console.log('exportTo plugin failed to copy css: ' + key);
+                            }
+                        }
+                    }
+                }
+                else{
+                    adopted.setAttribute(attr.name, attr.value);
+                }
+            }
+        }
 
-		return adopted
-	};
-	function exportTo(doc, i, el){
-		var adopted;
-		if (doc.importNode){
-			adopted = doc.importNode(el);
-		}
-		else{
-			adopted = importNode.call(doc, el)
-		}
-		this.push(adopted);
-	};
-	$.fn.extend({
-		embedCSS: function(){
-			return this.each(embedCSS);
-		},
-		exportTo: function(winOrDoc){
-			var ret = [], doc;
+        if (deep){
+            // copy the entire contents of the source element
+            adopted.innerHTML = externalNode.innerHTML;
+        }
 
-			if (winOrDoc.createElement){
-				doc = winOrDoc;
-			}
-			else if(winOrDoc.document && winOrDoc.document.createElement){
-				doc = winOrDoc.document;
-			}
-			else{
-				throw new window.TypeError();
-			}
-			
-			this.each(function(i, el){
-				exportTo.call(ret, doc, i, el);
-			});
-			
-			return $(ret);
-		}
-	});
+        return adopted
+    };
+    function exportTo(doc, i, el, deep){
+        var adopted;
+        if (doc.importNode){
+            adopted = doc.importNode(el, deep);
+        }
+        else{
+            adopted = importNode.call(doc, el, deep)
+        }
+        this.push(adopted);
+    };
+    $.fn.extend({
+        embedCSS: function(){
+            return this.each(embedCSS);
+        },
+        exportTo: function(winOrDoc, deep){
+            var retArray = [], doc, ret;
+
+            if (winOrDoc.createElement){
+                doc = winOrDoc;
+            }
+            else if(winOrDoc.document && winOrDoc.document.createElement){
+                doc = winOrDoc.document;
+            }
+            else{
+                throw new window.TypeError();
+            }
+
+            this.each(function(i, el){
+                exportTo.call(retArray, doc, i, el, deep);
+            });
+
+            ret = $(retArray);
+            ret.prevObject = this;
+            return ret;
+        }
+    });
 }(jQuery);
